9706df3b97abe08b77fc95bd0aa81f4696051dd9
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-11 09:13:09 +0200

    Add project files.

---
+++ 22, Static Attribute/Class2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22__Static_Attribute
+{
+    internal class Songs
+    {
+        public string Title;
+        public string Artist;
+        public int Duration;
+
+        public static int songcount = 0;
+        public Songs(string aTitle, string aArtist, int aDuration)
+        {
+            this.Title =aTitle;
+            this.Artist= aArtist;
+            this.Duration = aDuration;
+            songcount++;
+
+
+
+        }
+    }
+}
---
+++ 22, Static Attribute/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22__Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs holiday = new Songs("Holiday", "Green Day", 10);
+            Songs kashmir = new Songs("karshmir", "Led Zeppelin", 15);
+
+            Console.WriteLine(Songs.songcount);
+            Console.ReadLine();
+        }
+    }
+}
